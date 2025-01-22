7199553f6bf118ad495cb526a83a09b151d1dfa1
sai7673 <pcworld7673@gmail.com>
2025-01-22 11:12:21 +0530

    web is commited

---
+++ js/dom.js
@@ -0,0 +1,37 @@
+// DOCUMENT OBJECT MODEL 
+
+
+// it will represet the entire html document 
+// the movement we create html file and run it in the browser they immediately bcome members of dom     
+
+// html tag is the rool element of html 
+// document is the root element of dom
+
+// console.log(document);
+// console.log(document.all);
+
+// console.log(document.all[6]);
+
+// console.log(document.contentType);
+
+// console.log(document.links);
+
+// console.log(document.URL);
+
+// console.log(document.doctype);
+
+// console.log(document.head);
+
+// console.log(document.body);
+
+
+
+// create a DOM 
+
+let x = document.createElement("b")
+x.innerText = " im bold tag from javascript"
+document.body.append(x)
+
+
+
+// methods of dom 
